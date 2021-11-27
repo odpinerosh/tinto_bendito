9c035668cd45a0e2fe509a33723ab1263b11452b
odph <odpinerosh@gmail.com>
2021-11-26 20:13:51 -0500

    LoginActivity: added b_registrar

--- app/src/main/java/com/tintobendito/etb/SplashScreen.java
+++ app/src/main/java/com/tintobendito/etb/SplashScreen.java
@@ -2,12 +2,34 @@ package com.tintobendito.etb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        //Revisar si se tiene un Usuario en FireBase
+        FirebaseUser usuario = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (usuario == null) {
+            iniciarNuevaActividad(Login.class);
+        } else {
+            iniciarNuevaActividad(VerProductos_RecV.class);
+        }
+
+
+    }
+
+    private void iniciarNuevaActividad(Class clase) {
+        startActivity(new Intent(this, clase));
+        //Finalizar este Splash
+        finish();
     }
 }
